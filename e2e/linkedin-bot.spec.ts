0990e1e8a0fb1521406068fd7045804459c806cc
mbeave <39030128+mbeave@users.noreply.github.com>
2023-08-27 08:03:01 -0500

    implemented first iteration of csv writing system

--- e2e/linkedin-bot.spec.ts
+++ e2e/linkedin-bot.spec.ts
@@ -3,8 +3,11 @@ const fs = require('fs');
 
 const configFile = fs.readFileSync('config.json', 'utf-8');
 const config = JSON.parse(configFile);
+const filePath = config.csvFilePath;
+const d = new Date();
+const date = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
 
-test('test', async ({ page }) => {
+test('linkedin app bot', async ({ page }) => {
   var totalJobsApplied = 0;
   const searchTermArray = config.searchTerms;
   
@@ -53,141 +56,149 @@ test('test', async ({ page }) => {
     var jobsApplied = 0;
     var maxJobsApplied = 25;
     var jobsOnPage = 24;
+    var jobTitle = "";
+    var companyTitle = "";
     for (let h = 0; h < parseInt(pages); h++) {
       if (jobsApplied > maxJobsApplied) {
-          console.log("You applied to " + jobsApplied + " " + searchTerm + " jobs!");
-          totalJobsApplied += jobsApplied;
-          break;
+        console.log("You applied to " + jobsApplied + " " + searchTerm + " jobs!");
+        totalJobsApplied += jobsApplied;
+        break;
       }
       if (h != 0) {
-          let jobPage = jobsPerPage * h;
-          console.log("You've applied to " + totalJobsApplied + " jobs!");
-          console.log("Moving to next page...");
-          //await page.locator('ul.artdeco-pagination__pages > li').nth(h).click();
-          url = url + "&start=" + jobPage;
-          await page.goto(url);
-          await page.hover('.jobs-search-results-list');
-          for (let i = 0; i < 7; i++) {
-              await page.mouse.wheel(0, 400);
-          }
+        let jobPage = jobsPerPage * h;
+        console.log("You've applied to " + jobsApplied + " jobs!");
+        console.log("Moving to next page...");
+        //await page.locator('ul.artdeco-pagination__pages > li').nth(h).click();
+        url = url + "&start=" + jobPage;
+        await page.goto(url);
+        await page.hover('.jobs-search-results-list');
+        for (let i = 0; i < 7; i++) {
+          await page.mouse.wheel(0, 400);
+        }
       }
       jobsOnPage = await page.locator('.job-card-list__title').count();
       console.log(jobsOnPage + " jobs on this page");
       for (let i = 0; i < jobsOnPage; i++) {
-          try {
-              await page.hover('.jobs-search-results-list');
-              await page.mouse.wheel(0, 200);
-              await page.locator('.job-card-list__title').nth(i).click();
-              if (await page.isVisible('span.artdeco-button__text:has-text("Easy Apply")')) {
-                  await page.locator('span:has-text("Easy Apply")').first().click();
-              }
-              else {
-                  console.log("Already applied...");
-                  continue;
-              }
-              
-              page.waitForTimeout(8000);
-              if (await page.isVisible('span.artdeco-button__text:has-text("Submit")')) {
-                  //await page.locator('span:has-text("Choose")').first().click();
-                  await page.locator('label:has-text("Follow")').click({ timeout: 10000 });
-                  await page.locator('text=Submit Application').click();
-                  console.log("Job applied!");
-                  await jobsApplied++;
-                  await page.locator('.artdeco-button__icon').first().click();
-                  continue;
-              }
-              
-              await page.locator('span:has-text("Next")').first().click();
-              let progressValue = await page.locator("progress").first().getAttribute("Value")
-  
-              if (progressValue == "50") {  
-                  try {
-                      //await page.locator('span:has-text("Choose")').first().click();
-                      await page.locator('span:has-text("Review")').first().first().click();
-                      await page.locator('label:has-text("Follow")').click({ timeout: 10000 });
-                      await page.locator('text=Submit Application').click();
-                      console.log("Job applied!");
-                      await jobsApplied++;
-                      await page.locator('.artdeco-button__icon').first().click();
-                  } catch (e) {
-                      await page.locator('.artdeco-button__icon').first().click();
-                      await page.locator('span:has-text("Discard")').click();
-                      console.log("Can't apply");
-                      continue;
-                  }
-              }
-              else if (progressValue == "33") {
-                  try {
-                      //await page.locator('span:has-text("Choose")').first().click();
-                      await page.locator('span:has-text("Next")').first().click();
-                      await page.locator('span:has-text("Review")').first().first().click();
-                      if (await page.isVisible('span.artdeco-inline-feedback__message')) {
-                          let jobID = await page.locator('xpath=//div[@data-job-id]').nth(i).getAttribute('data-job-id');
-                          let href = "https://www.linkedin.com/jobs/view/" + jobID;
-                          console.log("Couldn't apply to job! Apply here: " + href);
-                          await page.locator('.artdeco-button__icon').first().click();
-                          await page.locator('span:has-text("Discard")').click();
-                          continue;
-                      }
-                      await page.locator('label:has-text("Follow")').click({ timeout: 10000 });
-                      await page.locator('text=Submit Application').click();
-                      console.log("Job applied!");
-                      await jobsApplied++;
-                      await page.locator('.artdeco-button__icon').first().click();
-                  } catch (e) {
-                      await page.locator('.artdeco-button__icon').first().click();
-                      await page.locator('span:has-text("Discard")').click();
-                      console.log("Can't apply");
-                      continue;
-                  }
-              }
-              else if (progressValue == "25") {
-                  try {
-                      //await page.locator('span:has-text("Choose")').first().click({timeout: 10000});
-                      await page.locator('span:has-text("Next")').first().click();
-                      if (await page.isVisible('span.artdeco-inline-feedback__message')) {
-                          let jobID = await page.locator('xpath=//div[@data-job-id]').nth(i).getAttribute('data-job-id');
-                          let href = "https://www.linkedin.com/jobs/view/" + jobID;
-                          console.log("Couldn't apply to job! Apply here: " + href);
-                          await page.locator('.artdeco-button__icon').first().click();
-                          await page.locator('span:has-text("Discard")').click();
-                          continue;
-                      }
-                      await page.locator('span:has-text("Next")').first().click();
-                      if (await page.isVisible('span.artdeco-inline-feedback__message')) {
-                          let jobID = await page.locator('xpath=//div[@data-job-id]').nth(i).getAttribute('data-job-id');
-                          let href = "https://www.linkedin.com/jobs/view/" + jobID;
-                          console.log("Couldn't apply to job! Apply here: " + href);
-                          await page.locator('.artdeco-button__icon').first().click();
-                          await page.locator('span:has-text("Discard")').click();
-                          continue;
-                      }
-                      await page.locator('span:has-text("Review")').first().click();
-                      await page.locator('label:has-text("Follow")').click({ timeout: 10000 });
-                      await page.locator('text=Submit Application').click();
-                      console.log("Job applied!");
-                      await jobsApplied++;
-                      await page.locator('.artdeco-button__icon').first().click();
-                  } catch (e) {
-                      await page.locator('.artdeco-button__icon').first().click();
-                      await page.locator('span:has-text("Discard")').click();
-                      console.log("Can't apply");
-                      continue;
-                  }
-              }
-              else {
-                  let jobID = await page.locator('xpath=//div[@data-job-id]').nth(i).getAttribute('data-job-id');
-                  let href = "https://www.linkedin.com/jobs/view/" + jobID;
-                  await page.locator('.artdeco-button__icon').first().click();
-                  console.log("5 pages or more. Moving on... Apply here if you would like: " + href);
-                  await page.locator('span:has-text("Discard")').click();
-                  continue;
-              }
-          } catch (e) {
-              await page.locator('.artdeco-button__icon').first().click();
-              console.log("Failed to apply to job");
-              continue;
+        try {
+          await page.hover('.jobs-search-results-list');
+          await page.mouse.wheel(0, 200);
+          await page.locator('.job-card-list__title').nth(i).click();
+          if (await page.isVisible('span.artdeco-button__text:has-text("Easy Apply")')) {
+            jobTitle = await page.locator('.jobs-unified-top-card__job-title').first().innerText();
+            companyTitle = await page.locator('.jobs-unified-top-card__primary-description > div > a').innerText();
+            await page.locator('span:has-text("Easy Apply")').first().click();
           }
+          else {
+            console.log("Already applied...");
+            continue;
+          }
+              
+          page.waitForTimeout(8000);
+          if (await page.isVisible('span.artdeco-button__text:has-text("Submit")')) {
+            //await page.locator('span:has-text("Choose")').first().click();
+            await page.locator('label:has-text("Follow")').click({ timeout: 10000 });
+            await page.locator('text=Submit Application').click();
+            console.log("Job applied!");
+            await jobsApplied++;
+            await page.locator('.artdeco-button__icon').first().click();
+            csvWriteLine(date, location, searchTerm, jobTitle, companyTitle, filePath);
+            continue;
+          }
+              
+          await page.locator('span:has-text("Next")').first().click();
+          let progressValue = await page.locator("progress").first().getAttribute("Value")
+
+          if (progressValue == "50") {  
+            try {
+              //await page.locator('span:has-text("Choose")').first().click();
+              await page.locator('span:has-text("Review")').first().first().click();
+              await page.locator('label:has-text("Follow")').click({ timeout: 10000 });
+              await page.locator('text=Submit Application').click();
+              console.log("Job applied!");
+              await jobsApplied++;
+              await page.locator('.artdeco-button__icon').first().click();
+              csvWriteLine(date, location, searchTerm, jobTitle, companyTitle, filePath);
+            } catch (e) {
+              await page.locator('.artdeco-button__icon').first().click();
+              await page.locator('span:has-text("Discard")').click();
+              console.log("Can't apply");
+              continue;
+            }
+          }
+          else if (progressValue == "33") {
+            try {
+              //await page.locator('span:has-text("Choose")').first().click();
+              await page.locator('span:has-text("Next")').first().click();
+              await page.locator('span:has-text("Review")').first().first().click();
+              if (await page.isVisible('span.artdeco-inline-feedback__message')) {
+                let jobID = await page.locator('xpath=//div[@data-job-id]').nth(i).getAttribute('data-job-id');
+                let href = "https://www.linkedin.com/jobs/view/" + jobID;
+                console.log("Couldn't apply to job! Apply here: " + href);
+                await page.locator('.artdeco-button__icon').first().click();
+                await page.locator('span:has-text("Discard")').click();
+                continue;
+              }
+              await page.locator('label:has-text("Follow")').click({ timeout: 10000 });
+              await page.locator('text=Submit Application').click();
+              console.log("Job applied!");
+              await jobsApplied++;
+              await page.locator('.artdeco-button__icon').first().click();
+              csvWriteLine(date, location, searchTerm, jobTitle, companyTitle, filePath);
+            } catch (e) {
+              await page.locator('.artdeco-button__icon').first().click();
+              await page.locator('span:has-text("Discard")').click();
+              console.log("Can't apply");
+              continue;
+            }
+          }
+          else if (progressValue == "25") {
+            try {
+              //await page.locator('span:has-text("Choose")').first().click({timeout: 10000});
+              await page.locator('span:has-text("Next")').first().click();
+              if (await page.isVisible('span.artdeco-inline-feedback__message')) {
+                let jobID = await page.locator('xpath=//div[@data-job-id]').nth(i).getAttribute('data-job-id');
+                let href = "https://www.linkedin.com/jobs/view/" + jobID;
+                console.log("Couldn't apply to job! Apply here: " + href);
+                await page.locator('.artdeco-button__icon').first().click();
+                await page.locator('span:has-text("Discard")').click();
+                continue;
+              }
+              await page.locator('span:has-text("Next")').first().click();
+              if (await page.isVisible('span.artdeco-inline-feedback__message')) {
+                let jobID = await page.locator('xpath=//div[@data-job-id]').nth(i).getAttribute('data-job-id');
+                let href = "https://www.linkedin.com/jobs/view/" + jobID;
+                console.log("Couldn't apply to job! Apply here: " + href);
+                await page.locator('.artdeco-button__icon').first().click();
+                await page.locator('span:has-text("Discard")').click();
+                continue;
+              }
+              await page.locator('span:has-text("Review")').first().click();
+              await page.locator('label:has-text("Follow")').click({ timeout: 10000 });
+              await page.locator('text=Submit Application').click();
+              console.log("Job applied!");
+              await jobsApplied++;
+              await page.locator('.artdeco-button__icon').first().click();
+              csvWriteLine(date, location, searchTerm, jobTitle, companyTitle, filePath);
+            } catch (e) {
+              await page.locator('.artdeco-button__icon').first().click();
+              await page.locator('span:has-text("Discard")').click();
+              console.log("Can't apply");
+              continue;
+            }
+          }
+          else {
+            let jobID = await page.locator('xpath=//div[@data-job-id]').nth(i).getAttribute('data-job-id');
+            let href = "https://www.linkedin.com/jobs/view/" + jobID;
+            await page.locator('.artdeco-button__icon').first().click();
+            console.log("5 pages or more. Moving on... Apply here if you would like: " + href);
+            await page.locator('span:has-text("Discard")').click();
+            continue;
+          }
+        } catch (e) {
+          await page.locator('.artdeco-button__icon').first().click();
+          console.log("Failed to apply to job");
+          continue;
+        }
       }
     }
   }
@@ -195,3 +206,18 @@ test('test', async ({ page }) => {
   page.waitForTimeout(100000);
   page.close();
 });
+
+
+async function csvWriteLine(date: string, location: string, searchTerm: string, jobTitle: string, companyTitle: string, filePath: string) {
+  let loc = location.split('%20').join(' ');
+  let search = searchTerm.split('%20').join(' ');
+  let jobData = [date, loc, search, jobTitle, companyTitle];
+  const jobCSV = jobData.join(',') + '\n';
+  fs.appendFile(filePath, jobCSV, err => {
+    if (err) {
+      console.error('Error writing CSV file:', err);
+    } else {
+      console.log('Job data saved.');
+    }
+  });
+}
